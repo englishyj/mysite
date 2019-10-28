9d14cc2bc7621fd9ebcfbb8d11b1f806ad428fa3
englishyj <441079498@qq.com>
2019-10-28 15:12:49 +0800

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+       .__                         .__
+______ |  |__   ____   ____   ____ |__|__  ___
+\____ \|  |  \ /  _ \_/ __ \ /    \|  \  \/  /
+|  |_> >   Y  (  <_> )  ___/|   |  \  |>    <
+|   __/|___|  /\____/ \___  >___|  /__/__/\_ \
+|__|        \/            \/     \/         \/
+@project:pywork
+@author:Phoenix
+@file:mysite
+@ide:PyCharm
+@time2:2019/10/28 14:02
+@month2:十月
+@email:datacraft@163.com
+"""
+from django.urls import path
+
+from blog import views
+
+app_name='blog'
+
+urlpatterns = [
+    path('',views.post_list,name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
+]
---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts=Post.published.all()
+    return render(request,'blog/post/list.html',{'posts':posts})
+
+def post_detail(request,year,month,day,post):
+    print(day)
+    post=get_object_or_404(Post,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+    return render(request,'blog/post/detail.html',{'posts':post})
