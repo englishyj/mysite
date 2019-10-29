b23272853bfcdf1d083895ab054fceb8fba28a05
englishyj <441079498@qq.com>
2019-10-29 17:54:24 +0800

    update20191028

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,6 @@ def post_list(request):
     return render(request,'blog/post/list.html',{'posts':posts,'page':page})
 
 def post_detail(request,year,month,day,post):
-    print(day)
     post=get_object_or_404(Post,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
     return render(request,'blog/post/detail.html',{'posts':post})
 
@@ -47,4 +46,4 @@ def post_share(request,post_id):
             sent=True
     else:
         form=EmailPostForm()
-    return render(request,'blog/post/share.html',{'post':post,'form':form})
+    return render(request,'blog/post/share.html',{'posts':post,'form':form,'sent':sent})
