5061a436fb1a7e5fe82607c219a6f34f478d000f
englishyj <441079498@qq.com>
2019-10-30 14:17:05 +0800

    update20191030

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -40,7 +41,12 @@ def post_detail(request,year,month,day,post):
             new_comment.save()
         else:
             comment_form=CommentForm()
-    return render(request,'blog/post/detail.html',{'posts':post,'comments':comments,'new_comment':new_comment,'comment_form':comment_form})
+    #相似文章
+    post_tags_ids=post.tags.values_list('id',flat=True)
+    similar_posts=Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts=similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    return render(request,'blog/post/detail.html',{'posts':post,'comments':comments,'new_comment':new_comment,
+                                                   'comment_form':comment_form,'similar_posts':similar_posts})
 
 
 class PostListView(ListView):
